56544bf37e891e018adb44b6d021f6d089d9f471
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-04 20:14:45 +0200

    click on Square changes state

--- src/Square.js
+++ src/Square.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './Square.css'
 
 class Square extends Component {
+  constructor() {
+    super();
+    this.state = {value: null};
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => alert('bau')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
