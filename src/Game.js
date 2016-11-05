f841034c5d17d834d6fcbcf0c1f6bf927aaa8d84
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-05 20:53:10 +0200

    Keep history

--- src/Game.js
+++ src/Game.js
@@ -7,16 +7,19 @@ class Game extends Component {
   constructor() {
     super();
     this.state = {
-      squares: Array(9).fill(null),
+      history: [
+        { squares: Array(9).fill(null) }
+      ],
       xIsNext: true
     }
   }
 
   render() {
+    const current = this.state.history[this.state.history.length-1];
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={this.state.squares} onClick={i => this.handleClick(i)} />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
           <div>{this.getStatus()}</div>
@@ -27,20 +30,21 @@ class Game extends Component {
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    const current = this.state.history[this.state.history.length-1];
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-
     squares[i] = this.currentPlayer();
     this.setState({
-      squares,
+      history: this.state.history.concat([{squares}]),
       xIsNext: !this.state.xIsNext
     });
   }
 
   getStatus() {
-    const winner = calculateWinner(this.state.squares);
+    const current = this.state.history[this.state.history.length-1];
+    const winner = calculateWinner(current.squares);
     if (winner) {
       return `Winner: ${winner}`;
     } else {
